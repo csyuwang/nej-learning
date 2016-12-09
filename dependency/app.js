fc362615aade61c0b2f42eb70d57ae5624499d00
csyuwang <csyuwang@sina.com>
2016-12-09 18:05:58 +0800

    add css and json injection

--- dependency/app.js
+++ dependency/app.js
@@ -2,8 +2,5 @@ NEJ.define([
     'pro/lib/a',
     '{pro}/lib/b.js'
 ],function(a,b){
-
-  var s = a.api() + '_' + b.api();
-  document.getElementById('example').innerHTML = s;
-
+  console.log(a.api() + '+' + b.api());
 });
